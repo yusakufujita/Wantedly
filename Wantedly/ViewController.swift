a735d382b2ddf153fdacf849599fc6a6b44eca5c
藤田優作 <marozaemon1217@gmail.com>
2021-03-05 05:48:53 +0900

    first commit

---
+++ Wantedly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wantedly
+//
+//  Created by 藤田優作 on 2021/03/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
