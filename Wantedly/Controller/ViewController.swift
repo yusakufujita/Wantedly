256c3cce60e551e78f06a3713309db89fce201d0
藤田優作 <marozaemon1217@gmail.com>
2021-03-05 16:42:45 +0900

    [add]UITableViewDataSourceを追加しました.

--- Wantedly/Controller/ViewController.swift
+++ Wantedly/Controller/ViewController.swift
@@ -7,15 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    
+class ViewController: UIViewController, UITableViewDataSource {
+
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: "TableViewCell", bundle: nil), forCellReuseIdentifier: "TableViewCell")
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell  = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath) as? TableViewCell else {
+            return UITableViewCell()
+        }
+    }
 }
+
 
