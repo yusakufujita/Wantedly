36b5fc65fdbbdab107557c5307eadf72b6396a7c
藤田優作 <marozaemon1217@gmail.com>
2021-03-05 16:49:40 +0900

    [add]UITableViewDelegateを追加しました。

--- Wantedly/Controller/ViewController.swift
+++ Wantedly/Controller/ViewController.swift
@@ -14,20 +14,30 @@ class ViewController: UIViewController, UITableViewDataSource {
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
+        tableView.delegate = self
         tableView.register(UINib(nibName: "TableViewCell", bundle: nil), forCellReuseIdentifier: "TableViewCell")
         // Do any additional setup after loading the view.
     }
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return 15
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell  = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath) as? TableViewCell else {
             return UITableViewCell()
         }
+        return cell
     }
 }
 
+
+extension ViewController:UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+         print("\(indexPath.row)番目のセルをタップ")
+     }
+    
+}
 
